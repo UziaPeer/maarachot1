bf18f290b8bedfeb981f24e8ddaf63891ae225bb
UziaPeer <uzia.Peer@msmail.ariel.ac.il>
2024-05-22 17:00:03 +0000

    Add files via upload
    update22may

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "NumClass.h"
+
+int main() {
+    int first;
+    int second;
+    //printf("Enter first number: ");
+    scanf("%d", &first);
+    //printf("Enter second number: ");
+    scanf("%d", &second);
+
+        printf("The Armstrong numbers are:");
+    for (int i = first; i <= second; i++) {
+        if (isArmstrong(i) == 1) {
+            printf(" %d", i);
+        }
+    }
+
+   
+
+    printf("\nThe Palindromes are:");
+    for (int i = first; i <= second; i++) {
+        if (isPalindrome(i) == 1) {
+            printf(" %d", i);
+        }
+    }
+ printf("\nThe Prime numbers are:");
+    for (int i = first; i <= second; i++) {
+        if (isPrime(i) == 1) {
+            printf(" %d", i);
+        }
+    }
+
+
+    printf("\nThe Strong numbers are:");
+    for (int i = first; i <= second; i++) {
+        if (isStrong(i) == 1) {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
